70287d50f1a649f804ae2909770b6564447efc2a
codehuman96 <anshumanjha1995@hotmail.com>
2018-07-12 17:31:27 +0530

    -Ans_12-07

--- src/com/abc/customer_one_system/MatchFormats.java
+++ src/com/abc/customer_one_system/MatchFormats.java
@@ -10,20 +10,32 @@ package com.abc.customer_one_system;
  * @author test
  */
 public class MatchFormats {
-
+    
+    String pattern="";
     boolean matchEmail(String email) {
-        String pattern="^[a-z][a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$";
-        if (email.matches(pattern)) {
-            return true;
-        }
-       return false;
+        pattern = "^[a-z][a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$";
+        return email.matches(pattern);
     }
-    boolean matchMobileNo(String num){
-        String pattern="^(\\+[0-9][0-9])?[0-9]{10}$";
-        if(num.matches(pattern)){
-            return true;
-        }
-        return false;
+
+    boolean matchMobileNo(String num) {
+        pattern = "^(\\+[0-9][0-9])?[0-9]{10}$";
+        return num.matches(pattern);
     }
+
+    boolean matchDOB(String date) {
+        pattern = "^[0-2][0-9]/[01][0-9]/[12][0-9]{3}$";
+        return date.matches(pattern);
+    }
+    boolean matchPIN(String pin){
+        pattern = "^[0-9]{6}$";
+        return pin.matches(pattern);
+    }
+    boolean matchPAN(String pan){
+        pattern ="[A-Z]{5}[0-9]{4}[A-Z]{1}";
+        return pan.matches(pattern);
+    }
+     
+            
+    
 
 }
