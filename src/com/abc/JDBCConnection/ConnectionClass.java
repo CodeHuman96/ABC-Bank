4f8f524b10d1225af736125ae5a3559094ae74c1
codehuman96 <anshumanjha1995@hotmail.com>
2018-07-18 12:47:06 +0530

    Anshuman

--- src/com/abc/JDBCConnection/ConnectionClass.java
+++ src/com/abc/JDBCConnection/ConnectionClass.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package JDBCConnection;
+package com.abc.JDBCConnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
