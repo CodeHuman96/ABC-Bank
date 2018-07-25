c8a67a3aaa7ba1d8a3fd02629830d1f7a77281cf
sampdatalwar <sampadatalwar@gmail.com>
2018-07-25 15:36:08 +0530

    sampada

--- src/com/abc/customer_one_system/ListOfCustomerRequests.java
+++ src/com/abc/customer_one_system/ListOfCustomerRequests.java
@@ -298,12 +298,7 @@ public class ListOfCustomerRequests extends javax.swing.JFrame {
             if(requestType.equals("Lost/Stolen Card"))
             {
 
-<<<<<<< HEAD
-=======
 
-           
-
->>>>>>> 25ab52da82de94970c65aa5d01a0278cdf125456
             try {
                 requestTypeVal = 3;
                 this.setVisible(false);
