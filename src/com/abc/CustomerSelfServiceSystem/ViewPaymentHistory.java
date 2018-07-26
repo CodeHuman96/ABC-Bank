d3cb2147af7054bd62fa0201c181d459eba4a762
nischala97 <nischala.g14@iiits.in>
2018-07-26 09:29:11 +0530

    updateBiller

--- src/com/abc/CustomerSelfServiceSystem/ViewPaymentHistory.java
+++ src/com/abc/CustomerSelfServiceSystem/ViewPaymentHistory.java
@@ -279,8 +279,10 @@ public class ViewPaymentHistory extends javax.swing.JFrame {
             Statement stmt=con.createStatement();
             rs=stmt.executeQuery(query);
         }
+        int flag=0;
         while(rs.next())
         {
+            flag++;
             String date=rs.getString(1);
             String acNo=rs.getString(2);
             String biller=rs.getString(3);
@@ -291,7 +293,9 @@ public class ViewPaymentHistory extends javax.swing.JFrame {
             model= (DefaultTableModel) tblHistory.getModel();
             model.addRow(new Object[]{date,acNo,biller,am,cat,stat});
             //System.out.println("inside while in getPayment method");
-        }        
+        }
+        if(flag==0)
+            lblMsg.setText("No data available");
     }
     private void txtToActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtToActionPerformed
     //    LocalDate todaydate = LocalDate.now();
